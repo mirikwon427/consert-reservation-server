e34dc6880ed4eba6e7fe007912c1e14d6fed1d12
권미리 <miri@gwonmiliui-MacBookPro.local>
2025-06-06 22:47:15 +0900

    feat: ConcertDate 엔티티 클래스 및 컬럼 정의

---
+++ src/main/java/kr/hhplus/be/server/concert/ConcertDate.java
@@ -0,0 +1,41 @@
+package kr.hhplus.be.server.concert;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import kr.hhplus.be.server.BaseTimeEntity;
+
+@Entity
+public class ConcertDate extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "concert_date_id")
+  private Long id;
+
+  @Column(name = "concert_id", nullable = false)
+  private Long concertId;
+
+  @Column(name = "concert_date", nullable = false)
+  private LocalDate concertDate;
+
+  @Column(name = "start_time", nullable = false)
+  private LocalDateTime startTime;
+
+  @Column(name = "end_time", nullable = false)
+  private LocalDateTime endTime;
+
+  protected ConcertDate() {}
+
+  public ConcertDate(Long id, Long concertId, LocalDate concertDate, LocalDateTime startTime, LocalDateTime endTime) {
+    this.id = id;
+    this.concertId = concertId;
+    this.concertDate = concertDate;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
+}
